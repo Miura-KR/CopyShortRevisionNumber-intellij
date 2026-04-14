5b1cf9c22f0833af5aaa6455d85d38f61ca521ca
Miura-KR <ovis_w6v6_grappler95@yahoo.co.jp>
2026-04-14 22:42:28 +0900

    v1.0.1リリース

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.k.pmpstudy"
-version = "1.0.0"
+version = "1.0.1"
 
 // Set the JVM language level used to build the project.
 kotlin {
@@ -36,7 +36,7 @@ intellijPlatform {
         }
 
         changeNotes = """
-            Initial version
+            Compatibility update.
         """.trimIndent()
     }
 }
